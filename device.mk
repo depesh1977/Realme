e4278c1197317e6dc681b624e117ee9b35b2d6aa
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-01-09 15:13:07 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,85 +7,40 @@
 
 LOCAL_PATH := device/realme/RMX3231
 
-PRODUCT_PLATFORM := sp9863a
-
-# Dynamic Partitions stuff
+# Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-
-PRODUCT_PROPERTY_OVERRIDES += ro.apex.updatable=false
-
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    uboot \
-    sml \
-    trustos \
-    teecfg \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vbmeta_product \
-    vbmeta_system_ext \
-    dtbo \
-    l_ldsp \
-    l_gdsp \
-    l_modem \
-    l_deltanv \
-    wcnmodem \
-    gpsgl \
-    gpsbd \
-    pm_sys \
-    boot \
-    system \
-    system_ext \
-    vendor \
-    product \
-    socko \
-    odmko
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
-
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    checkpoint_gc
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-# Health Hal
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service
+    
+# Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+PRODUCT_PACKAGES += \
+    bootctrl.unisoc \
+    bootctrl.unisoc.recovery
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
 # Fastbootd
 PRODUCT_PACKAGES += \
+    fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
-
+    android.hardware.fastboot@1.0-impl-mock.recovery
