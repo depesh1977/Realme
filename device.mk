146448fc1a14f4bcce9e03044d34860d10abfca9
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-26 02:59:34 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,20 +1,26 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/realme/RMX3231
+LOCAL_PATH := device/motorola/java
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    dtbo \
+    boot \
+    system \
+    system_ext \
+    vendor \
+    product
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -22,10 +28,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-# VIRTUAL A/b
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -35,8 +37,13 @@ PRODUCT_PACKAGES += \
     
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.ums512 \
+    bootctrl.ums512.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -47,4 +54,6 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
