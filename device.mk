433af652c412f9d1da07a8983ed47ae8a55c0598
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-25 19:53:19 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -47,9 +47,4 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+    fastbootd
