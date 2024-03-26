cc7563f92184e65286472f340d611d44386b1f9f
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-26 03:03:50 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,13 +1,10 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Virtual AB
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-LOCAL_PATH := device/motorola/java
+# API level
+PRODUCT_SHIPPING_API_LEVEL := 30
 
-# Dynamic Partitions
+# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 AB_OTA_UPDATER := true
@@ -22,38 +19,22 @@ AB_OTA_PARTITIONS += \
     vendor \
     product
 
-# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service
-    
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.ums512 \
-    bootctrl.ums512.recovery
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
     update_engine \
-    update_verifier \
+    update_verifier
+
+PRODUCT_PACKAGES += \
     update_engine_sideload
 
-# Fastbootd
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 PRODUCT_PACKAGES += \
-    fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+	fastbootd
