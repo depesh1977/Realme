98561b890952680db2d5c87146dda19827715335
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-26 16:11:47 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,16 +25,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-PRODUCT_PACKAGES_DEBUG += \
-update_engine_client
-
 PRODUCT_PACKAGES += \
 otapreopt_script \
 cppreopts.sh \
 update_engine \
 update_verifier \
 update_engine_sideload
-
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
