8178f523142a36a863f8edc397820632b3e4c16a
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-30 00:39:30 +0300

    Update twrp_RMX3231.mk

--- twrp_RMX3231.mk
+++ twrp_RMX3231.mk
@@ -6,8 +6,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
