40706751fa1cd6e4e231a011b811202c088ec3d6
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-03-30 22:13:18 +0300

    Update twrp_RMX3231.mk

--- twrp_RMX3231.mk
+++ twrp_RMX3231.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from RMX3231 device
 $(call inherit-product, device/realme/RMX3231/device.mk)
